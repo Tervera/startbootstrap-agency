edffbcb86e16d0c197836af6db410abe153f8c3e
Aosu Terver <terveraosu@gmail.com>
2019-02-16 14:11:55 +0100

    fixed issue with subscription form on mobile view

--- mail/subscribe.php
+++ mail/subscribe.php
@@ -11,7 +11,7 @@ $email_address = strip_tags(htmlspecialchars($_POST['email']));
    
 // Create the email and send the message
 $to = 'subscribe@truetech.com.ng'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Subscription";
+$email_subject = "TrueTech Subscription";
 $email_body = "Inform me when you are up and runing";
 $headers = "From: noreply@truetech.com.ng\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
